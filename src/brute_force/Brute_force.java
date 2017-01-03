d5d39159a4345b163c71e86baca08ba60ff80cc6
Jem_96 <jeamathuran@ymail.com>
2017-01-03 23:24:11 +0000

    Code Improvement

--- src/brute_force/Brute_force.java
+++ src/brute_force/Brute_force.java
@@ -5,42 +5,40 @@ import java.util.Random;
 public class Brute_force {
 
     public static void main(String[] args) {
-        for (int a = 30; a < 40; a++) {
+        int bit = 10;
+        for (int n = 1; n < 30; n++) {
             long startTime = System.nanoTime();
-            //System.out.print(begin(a) + " ");
-            begin(a);
-            System.out.println((System.nanoTime() - startTime));
+            for (int i = 0; i < 100; i++) {
+                //System.out.print(begin(a) + " ");
+                begin(n, bit);
+            }
+            System.out.println((System.nanoTime() - startTime) / 100);
         }
     }
 
-    public static boolean begin(int size) {
-        int[] array = new int[size];
+    public static boolean begin(int n, int bit) {
+        int max_n_bit_number = (int) Math.pow(2, bit - 1);
+        int[] S = new int[n];
         Random rand = new Random();
-        int n;
-        for (int i = 0; i < size; i++) {
-            n = rand.nextInt(100) + 1;
-            array[i] = n;
+        int temp;
+        for (int i = 0; i < n; i++) {
+            temp = rand.nextInt(max_n_bit_number) + 0;
+            S[i] = temp;
         }
-
-        int sum = 1200;
-        int m = array.length;
-        if (SubsetSum_Brute_Force(array, m, sum) == true) {
-            return true;
-        } else {
-            return false;
-        }
+        int t = rand.nextInt(n * max_n_bit_number) + 0;
+        return SubsetSum_Brute_Force(S, n, t) == true;
     }
 
-    public static boolean SubsetSum_Brute_Force(int[] array, int m, int sum) {
-        if (sum == 0) {
+    public static boolean SubsetSum_Brute_Force(int[] S, int n, int t) {
+        if (t == 0) {
             return true;
         }
-        if (m == 0 && sum != 0) {
+        if (n == 0 && t != 0) {
             return false;
         }
-        if (array[m - 1] > sum) {
-            return SubsetSum_Brute_Force(array, m - 1, sum);
+        if (S[n - 1] > t) {
+            return SubsetSum_Brute_Force(S, n - 1, t);
         }
-        return SubsetSum_Brute_Force(array, m - 1, sum) || SubsetSum_Brute_Force(array, m - 1, sum - array[m - 1]);
+        return SubsetSum_Brute_Force(S, n - 1, t) || SubsetSum_Brute_Force(S, n - 1, t - S[n - 1]);
     }
 }
