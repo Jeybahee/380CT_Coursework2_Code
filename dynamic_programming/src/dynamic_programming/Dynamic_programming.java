9d6bd7c4d7af96c48aa600706d77cae41876b4aa
Jeybahee <Jeamathuran@ymail.com>
2017-01-04 00:49:19 +0000

    Commit Successfully!

--- dynamic_programming/src/dynamic_programming/Dynamic_programming.java
+++ dynamic_programming/src/dynamic_programming/Dynamic_programming.java
@@ -5,50 +5,49 @@ import java.util.Random;
 public class Dynamic_programming {
 
     public static void main(String[] args) {
-        for (int a = 20; a < 1000; a = a + 40) {
+        int bit = 10;
+        for (int n = 1; n < 200; n++) {
             long startTime = System.nanoTime();
-            //System.out.print(begin(a) + " ");
-            begin(a);
-            System.out.println((System.nanoTime() - startTime));
+            for (int i = 0; i < 100; i++) {
+                //System.out.print(begin(a) + " ");
+                begin(n, bit);
+            }
+            System.out.println((System.nanoTime() - startTime) / 100);
         }
     }
 
-    public static boolean begin(int size) {
-        int[] array = new int[size];
+    public static boolean begin(int n, int bit) {
+        int max_n_bit_number = (int) Math.pow(2, bit - 1);
+        int[] S = new int[n];
         Random rand = new Random();
-        int n;
-        for (int i = 0; i < size; i++) {
-            n = rand.nextInt(100) + 1;
-            array[i] = n;
+        int temp;
+        for (int i = 0; i < n; i++) {
+            temp = rand.nextInt(max_n_bit_number) + 0;
+            S[i] = temp;
         }
-        int sum = 1700;
-        int m = array.length;
-        if (SubsetSum_Dynamic_Programming(array, m, sum) == true) {
-            return true;
-        } else {
-            return false;
-        }
+        int t = rand.nextInt(n * max_n_bit_number) + 0;
+        return SubsetSum_Dynamic_Programming(S, n, t) == true;
     }
 
-    public static boolean SubsetSum_Dynamic_Programming(int[] array, int m, int sum) {
-        boolean subset[][] = new boolean[m + 1][sum + 1];
-        for (int i = 1; i <= sum; i++) {
+    public static boolean SubsetSum_Dynamic_Programming(int[] S, int n, int t) {
+        boolean subset[][] = new boolean[n + 1][t + 1];
+        for (int i = 1; i <= t; i++) {
             subset[0][i] = false;
         }
 
-        for (int i = 0; i <= m; i++) {
+        for (int i = 0; i <= n; i++) {
             subset[i][0] = true;
         }
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= sum; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= t; j++) {
                 subset[i][j] = subset[i - 1][j];
-                if (subset[i][j] == false && j >= array[i - 1]) {
+                if (subset[i][j] == false && j >= S[i - 1]) {
                     subset[i][j] = subset[i][j]
-                            || subset[i - 1][j - array[i - 1]];
+                            || subset[i - 1][j - S[i - 1]];
                 }
             }
         }
-        return subset[m][sum];
+        return subset[n][t];
     }
 }
