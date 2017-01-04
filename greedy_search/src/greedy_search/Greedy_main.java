86deba1d657b02c021438556f0ad496307c53aeb
Jeybahee <Jeamathuran@ymail.com>
2017-01-04 00:50:10 +0000

    Commit Successfully!

--- greedy_search/src/greedy_search/Greedy_main.java
+++ greedy_search/src/greedy_search/Greedy_main.java
@@ -1,48 +1,49 @@
 package greedy_search;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 public class Greedy_main {
 
     public static void main(String[] args) {
-
-        for (int a = 20; a < 100000; a = a * 2) {
+        int bit = 20;
+        for (int n = 1; n < 200; n++) {
             long startTime = System.nanoTime();
-            //System.out.print(beginGreedy(a) + " ");
-            begin(a);
-            System.out.println((System.nanoTime() - startTime));
+            for (int i = 0; i < 100; i++) {
+                //System.out.print(beginGreedy(a) + " ");
+                begin(n, bit);
+            }
+            System.out.println((System.nanoTime() - startTime) / 100);
             //System.out.println("(" + status + ") took " + ((System.nanoTime() - startTime) / 100) + " nano second(s).");
         }
     }
 
-    public static boolean begin(int size) {
-        int[] array = new int[size];
+    public static boolean begin(int n, int bit) {
+        int max_n_bit_number = (int) Math.pow(2, bit - 1);
+        Integer[] S = new Integer[n];
         Random rand = new Random();
-        for (int i = 0; i < size; i++) {
-            array[i] = rand.nextInt(1000) + 1;
+        int temp;
+        for (int i = 0; i < n; i++) {
+            temp = rand.nextInt(max_n_bit_number) + 0;
+            S[i] = temp;
         }
-
-        int sum = 1700;
-        int len = array.length;
-
-        if (SubsetSum_Greedy(array, len, sum) == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        int t = rand.nextInt(n * max_n_bit_number) + 0;
+        Arrays.sort(S, Collections.reverseOrder());
+        return SubsetSum_Greedy(S, n, t) == 0;
     }
 
-    public static int SubsetSum_Greedy(int array[], int len, int sum) {
+    public static int SubsetSum_Greedy(Integer S[], int n, int t) {
         int total = 0;
         int i = 0;
 //        List subset = new ArrayList<>();
 
-        while (i <= len - 1) {
-            if (total + array[i] <= sum) {
-                total = total + array[i];
+        while (i <= n - 1) {
+            if (total + S[i] <= t) {
+                total = total + S[i];
 //                subset.add(array[i]);
             }
-            if(total == sum) {
+            if (total == t) {
                 break;
             }
             i++;
@@ -51,6 +52,6 @@ public class Greedy_main {
 //        for (int a = 0; a < subset.size(); a++) {
 //            System.out.print(subset.get(a).toString() + " ");
 //        }
-        return sum - total;
+        return t - total;
     }
 }
